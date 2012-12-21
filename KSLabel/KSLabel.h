303bc8a7f9a7be56d834202a2e61e4d616334270
vigorouscoding <kai@vigorouscoding.com>
2012-12-21 17:42:29 +0100

    fixed an error where the textColor get set to white and added some more overall flexibility to KSLabel

--- KSLabel/KSLabel.h
+++ KSLabel/KSLabel.h
@@ -7,28 +7,28 @@
 
 //  Licensing (Zlib)
 //
- // This software is provided 'as-is', without any express or implied
- // warranty. In no event will the authors be held liable for any damages
- // arising from the use of this software.
+//  This software is provided 'as-is', without any express or implied
+//  warranty. In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software in
+//     a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not
+//     be misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
 
- // Permission is granted to anyone to use this software for any purpose,
- // including commercial applications, and to alter it and redistribute it
- // freely, subject to the following restrictions:
-
- // 1. The origin of this software must not be misrepresented; you must not
- //    claim that you wrote the original software. If you use this software in
- //    a product, an acknowledgment in the product documentation would be
- //    appreciated but is not required.
-
- // 2. Altered source versions must be plainly marked as such, and must not
- //    be misrepresented as being the original software.
-
- // 3. This notice may not be removed or altered from any source distribution.
-
- // If you are using this code, you might want to consider giving some credit to me by
-	// 1) linking my website (http://www.vigorouscoding.com) from your app's website 
-	// 2) or crediting me inside the app's credits page 
-	// 3) or a tweet mentioning @modogo
+//  If you are using this code, you might want to consider giving some credit to me by
+//	1) linking my website (http://www.vigorouscoding.com) from your app's website 
+//	2) or crediting me inside the app's credits page 
+//	3) or a tweet mentioning @modogo
 
 #import <UIKit/UIKit.h>
 
